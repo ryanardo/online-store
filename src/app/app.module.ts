91483ab7bf9a383cb83d5a10da84efd5d5b66251
Ryan G Ferris <ryanglenferris@gmail.com>
2018-02-27 10:35:42 -0800

    Update & Delete
    Create logic for updating & deleting data.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,30 +2,42 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { routing } from './app.routing';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
-import { PipeNamePipe } from './pipe-name.pipe';
-
-import { routing } from './app.routing';
 import { AboutComponent } from './about/about.component';
 import { MarketplaceComponent } from './marketplace/marketplace.component';
 import { AlbumDetailComponent } from './album-detail/album-detail.component';
+import { masterFirebaseConfig } from './api-keys';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AdminComponent } from './admin/admin.component';
+import { EditAlbumComponent } from './edit-album/edit-album.component';
+
+export const firebaseConfig = {
+	apiKey: masterFirebaseConfig.apiKey,
+	authDomain: masterFirebaseConfig.authDomain,
+	databaseURL: masterFirebaseConfig.databaseURL,
+	storageBucket: masterFirebaseConfig.storageBucket
+};
 
 @NgModule({
 	declarations: [
 		AppComponent,
 		WelcomeComponent,
-		PipeNamePipe,
 		AboutComponent,
 		MarketplaceComponent,
-		AlbumDetailComponent
+		AlbumDetailComponent,
+		AdminComponent,
+		EditAlbumComponent
 	],
 	imports: [
 		BrowserModule,
 		FormsModule,
 		HttpModule,
-		routing
+		routing,
+		AngularFireModule.initializeApp(firebaseConfig),
+		AngularFireDatabaseModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
